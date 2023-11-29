5ba7f3f0ee27eacd1d79d98f06e130d1e8a9a30a
Altyrion69 <altyrion@hotmail.fr>
2023-11-29 10:57:28 +0100

    fixtures9

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -12,13 +12,13 @@ class CategoryFixtures extends Fixture
       'Action',
       'Aventure',
       'Animation',
-      'Fantasique',
+      'Fantastique',
       'Horreur',
     ];
 
     public function load(ObjectManager $manager)
     {
-      foreach (self::CATEGORIES as $key => $categoryName) {
+      foreach (self::CATEGORIES as $categoryName) {
         $category = new Category();
         $category->setName($categoryName);
 
